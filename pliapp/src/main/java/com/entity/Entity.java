eae2e8634e53c2638fb4d21ff8f093c7a40053ad
faisal <clickfaisalamine@gmail.com>
2025-08-25 20:42:37 +0600

    pli_app

---
+++ pliapp/src/main/java/com/entity/Entity.java
@@ -0,0 +1,38 @@
+package com.entity;
+
+public class Entity {
+	
+	private int id;
+	private String name;
+	private String course;
+	private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Entity [id=" + id + ", name=" + name + ", course=" + course + ", status=" + status + "]";
+	}
+
+}
